1329c15e851c103463d4e9381043bb02ffafee69
SupquS <os25693@gmail.com>
2016-09-28 00:46:28 +0300

    Add cool in style.css for table

--- php homework/multiplication table/index.php
+++ php homework/multiplication table/index.php
@@ -2,24 +2,39 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Таблица умножения</title>
+    <link rel="stylesheet" href="style.css">
+
+    <title>Multiplication Table</title>
 </head>
 <body>
-<table border="1" cellspacing="1" cellpadding="5">
+<table>
     <thead>
     <tr>
-        <th>Таблица умножения </th>
+        <th colspan="8">Multiplication Table</th>
     </tr>
     </thead>
     <tbody>
-        <?php
-        for($a = 2; $a <= 9; $a++){
-            for($b = 1; $b <=9; $b++){
-                $c = $a * $b;
-                echo "<tr><td>$a * $b = $c</td></tr>";
-            }
+    <tr>
+        <th>2 Times Table</th>
+        <th>3 Times Table</th>
+        <th>4 Times Table</th>
+        <th>5 Times Table</th>
+        <th>6 Times Table</th>
+        <th>7 Times Table</th>
+        <th>8 Times Table</th>
+        <th>9 Times Table</th>
+    </tr>
+    <?php
+    for($b = 1; $b <= 9; $b++){
+        $a = 2;
+        $c = $a * $b;
+        echo "<tr><td>"."$a * $b = $c"."</td>";
+        for($a = 3; $a <= 9; $a++){
+            $c = $a * $b;
+            echo "<td>"."$a * $b = $c"."</td>";
         }
-        ?>
+    }
+    ?>
     </tbody>
 </table>
 </body>
